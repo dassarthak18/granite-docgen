daccb969ed0316a46c2b5c180924d9a4d30031b9
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-08-10 01:46:41 +0530

    Update docgen.py

--- src/docgen.py
+++ src/docgen.py
@@ -1,4 +1,4 @@
-import os
+import os, shutil
 import torch
 from langchain.chains import RetrievalQA
 from langchain_huggingface import HuggingFaceEmbeddings
@@ -20,7 +20,7 @@ else:
     LOCAL_REPO_PATH = fetch_repo(REPO_URL, BRANCH_OR_COMMIT, github_token=GITHUB_TOKEN)
 
 prepare_codebase_for_vectors(LOCAL_REPO_PATH)
-os.rmdir(LOCAL_REPO_PATH)
+shutil.rmtree(LOCAL_REPO_PATH)
 
 embeddings = HuggingFaceEmbeddings(
         model_name="all-MiniLM-L6-v2",
