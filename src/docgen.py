e22c363957cc9ce96cda766aa5c23e66b64731ba
Sarthak Das <68112450+dassarthak18@users.noreply.github.com>
2025-08-10 15:35:02 +0530

    Update docgen.py

--- src/docgen.py
+++ src/docgen.py
@@ -2,7 +2,7 @@ import os, shutil
 import torch
 from langchain.chains import RetrievalQA
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain.llms import HuggingFacePipeline
+from langchain.llms import HuggingFacePipeline, LlamaCpp
 from langchain.vectorstores import FAISS
 from transformers import pipeline
 from parser import fetch_repo, prepare_codebase_for_vectors
@@ -18,13 +18,14 @@ if not GITHUB_TOKEN:
     LOCAL_REPO_PATH = fetch_repo(REPO_URL, BRANCH_OR_COMMIT)
 else:
     LOCAL_REPO_PATH = fetch_repo(REPO_URL, BRANCH_OR_COMMIT, github_token=GITHUB_TOKEN)
+USE_GPU = os.environ.get("USE_GPU", "0")
 
 prepare_codebase_for_vectors(LOCAL_REPO_PATH)
 shutil.rmtree(LOCAL_REPO_PATH)
 
 embeddings = HuggingFaceEmbeddings(
         model_name="all-MiniLM-L6-v2",
-        model_kwargs={"device": "cuda" if os.environ.get("USE_GPU", "0") == "1" else "cpu"},
+        model_kwargs={"device": "cuda" if USE_GPU == "1" else "cpu"},
         encode_kwargs={"batch_size": 64}
 )
 
@@ -34,18 +35,21 @@ retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k
 model_path = "./granite"
 device = 0 if torch.cuda.is_available() else -1
 
-pipe = pipeline(
-    "text-generation",
-    model=model_path,
-    tokenizer=model_path,
-    device=device,
-    max_length=128000,
-    do_sample=True,
-    temperature=0.7,
-    top_p=0.9,
-)
+if device == -1:
+    llm = LlamaCpp(model_path=model_path, n_ctx=2048, temperature=0.8, top_p=0.8)
+else:
+    pipe = pipeline(
+        "text-generation",
+        model=model_path,
+        tokenizer=model_path,
+        device=device,
+        max_length=2048,
+        do_sample=True,
+        temperature=0.8,
+        top_p=0.8,
+    )
+    llm = HuggingFacePipeline(pipeline=pipe)
 
-llm = HuggingFacePipeline(pipeline=pipe)
 qa_chain = RetrievalQA.from_chain_type(llm=llm, retriever=retriever, return_source_documents=False)
 
 with open("query.txt", "r", encoding="utf-8") as f:
